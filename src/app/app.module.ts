b248e1337a1bc5cd4ef501237ed2253ce9eee47a
rishabh-malhotra <rishabh11.rm@gmail.com>
2020-05-02 19:18:10 +0530

    added initial skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewsComponent } from './news/news.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { PrecautionsComponent } from './precautions/precautions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    NewsComponent,
+    AdminLoginComponent,
+    PrecautionsComponent
   ],
   imports: [
     BrowserModule,
