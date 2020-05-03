e810db485938faa04e6d81401c4a785dd4853741
rishabh-malhotra <rishabh11.rm@gmail.com>
2020-05-03 23:04:21 +0530

    added news

--- src/app/services/news-service/news.service.ts
+++ src/app/services/news-service/news.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { NewsDataModel } from '../../models/news-data-model.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NewsService {
 
+  newsList: NewsDataModel[] = [];
+
   constructor() { }
+
+  addNews(news: NewsDataModel) {
+    try {
+      //this.newsList = localStorage.getItem('newsDetails');
+      this.newsList.push(news);
+      //localStorage.setItem('newsDetails',this.newsList);
+    }
+    catch (error) {
+        console.log(error);
+    }
+  }
 }
+
